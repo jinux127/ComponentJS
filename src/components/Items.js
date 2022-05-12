8f483ea1b23f3b08f47a8921974d575d044583e6
jinwoo <wwo1278@gmail.com>
2022-05-12 20:13:38 +0900

    이벤트를 추가할 때의 불편함

--- src/components/Items.js
+++ src/components/Items.js
@@ -8,16 +8,31 @@ export default class Items extends Component {
     const { items } = this.$state;
     return `
             <ul>
-                ${items.map((item) => `<li>${item}</li>`).join('')}
+                ${items
+                  .map(
+                    (item, key) => `
+                <li>${item}</li>
+                <button class="deleteButton" data-index="${key}">삭제</button>
+                `
+                  )
+                  .join('')}
             </ul>
-            <button>추가</button>
+            <button class="addButton">추가</button>
         `;
   }
 
   setEvent() {
-    this.$target.querySelector('button').addEventListener('click', () => {
+    this.$target.querySelector('.addButton').addEventListener('click', () => {
       const { items } = this.$state;
       this.setState({ items: [...items, `item${items.length + 1}`] });
+    });
+
+    this.$target.querySelectorAll('.deleteButton').forEach((deleteBtn) => {
+      deleteBtn.addEventListener('click', ({ target }) => {
+        const items = [...this.$state.items];
+        items.splice(target.dataset.index, 1);
+        this.setState({ items });
+      });
     });
   }
 }
