8bc4a8e52320894502a57d60d87a55c5baebcef6
jinwoo <wwo1278@gmail.com>
2022-05-12 20:06:34 +0900

    바닐라 JS로 웹 컴포넌트 만들기

---
+++ src/Component.js
@@ -0,0 +1,28 @@
+export default class Component {
+  $target;
+  $state;
+
+  constructor($target) {
+    this.$target = $target;
+    this.setup();
+    this.render();
+  }
+
+  setup() {}
+
+  template() {
+    return '';
+  }
+
+  setEvent() {}
+
+  setState(nextState) {
+    this.$state = { ...this.$state, ...nextState };
+    this.render();
+  }
+
+  render() {
+    this.$target.innerHTML = this.template();
+    this.setEvent();
+  }
+}
